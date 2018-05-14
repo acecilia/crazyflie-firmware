8be705b67e09dd2a8bf4b19de761d17202c11fcc
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-05-14 23:09:35 +0200

    Clean

--- src/modules/src/libdict_test.c
+++ src/modules/src/libdict_test.c
@@ -33,31 +33,31 @@ static char* getName(dictionary_t type) {
 }
 
 /**
- Printing tests
+ Printing tests. Requires disabling the watchdog.
+ Notes: I have observed that running this in DEBUG mode leads to the crazyflie crashing, but running it in release mode with the watchdog disabled works fine. To disable the watchdog, look for calls to the function "watchdogInit" and comment them
  */
 void test_libdict() {
   int length = sizeof(type_array) / sizeof(dictionary_t);
 
   configure_dict_malloc();
 
-  DEBUG_PRINT("### Starting libdict tests:\n");
-
+  DEBUG_PRINT("\n#### Starting libdict tests:\n");
   DEBUG_PRINT("[Mem: %d bytes]\n", xPortGetFreeHeapSize());
 
-  DEBUG_PRINT("### Insert/search test:\n");
+  DEBUG_PRINT("\n### Insert/search test:\n");
   for(int i = 0; i < length; i++) {
-    char* r1 = test_uint8_insert_search(type_array[i]) ? "Ok" : "Failed";
-    DEBUG_PRINT("%s [%s]\n", r1, getName(type_array[i]));
+    char* result = test_uint8_insert_search(type_array[i]) ? "Ok" : "Failed";
+    DEBUG_PRINT("%s [%s]\n", result, getName(type_array[i]));
   }
-
   DEBUG_PRINT("[Mem: %d bytes]\n", xPortGetFreeHeapSize());
 
   int benchmark_duration_ms = 5000;
-  DEBUG_PRINT("### Insert benchmark (%d ms):\n", benchmark_duration_ms);
+  DEBUG_PRINT("\n### Insert benchmark (%d ms):\n", benchmark_duration_ms);
   for(int i = 0; i < length; i++) {
-    int r1 = benchmark_uint_keys_dict_insert(type_array[i], benchmark_duration_ms);
-    DEBUG_PRINT("%d [%s]\n", r1, getName(type_array[i]));
+    int result = benchmark_uint_keys_dict_insert(type_array[i], benchmark_duration_ms);
+    DEBUG_PRINT("%d [%s]\n", result, getName(type_array[i]));
   }
-
   DEBUG_PRINT("[Mem: %d bytes]\n", xPortGetFreeHeapSize());
+
+  DEBUG_PRINT("\n#### Finished libdict tests\n\n");
 }
