2b3c3e6abf65bafe9102e189b7eb8fd95b1cab93
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-05-14 22:33:53 +0200

    Fix previous commit

--- src/modules/interface/libdict_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef libdict_test_h
-#define libdict_test_h
-
-#include "libdict.h"
-
-int benchmark_uint_keys_dict_insert_remove(dictionary_t type, int ms);
-bool test_uint8_insert_search(dictionary_t type);
-
-#endif /* libdict_test_h */
