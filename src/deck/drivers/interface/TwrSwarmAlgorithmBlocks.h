65ed019690b97564589d008b0f5f95935d23bf95
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-05-23 22:46:02 +0200

    Added TwrSwarmAlgorithmBlocks, to allow internal implementation code-blocks to be tested

---
+++ src/deck/drivers/interface/TwrSwarmAlgorithmBlocks.h
@@ -0,0 +1,12 @@
+#ifndef TwrSwarmAlgorithmBlocks_h
+#define TwrSwarmAlgorithmBlocks_h
+
+#include "TwrSwarmAlgorithm.h"
+
+uint32_t adjustTxRxTime(dwTime_t *time);
+dwTime_t findTransmitTimeAsSoonAsPossible(dwDevice_t *dev);
+double calculateClockCorrection(uint32_t prevRemoteTx, uint32_t remoteTx, uint32_t prevLocalRx, uint32_t localRx);
+neighbourData_t* getDataForNeighbour(dict* dct, locoAddress_t address);
+unsigned int createTxPacket(lpsSwarmPacket_t** txPacketPointer, dict* dct, locoAddress_t sourceAddress, uint32_t localTx);
+
+#endif /* TwrSwarmAlgorithmBlocks_h */
