c9f76e33dec40c0b75b0718a02c30ef995961574
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-07-12 18:24:44 +0200

    Bring back noise step API improvements

--- src/utils/interface/estimatorKalman/estimatorKalmanEngine.h
+++ src/utils/interface/estimatorKalman/estimatorKalmanEngine.h
@@ -10,8 +10,8 @@ typedef struct {
   void (*update)(estimatorKalmanStorage_t* storage);
 
   // Incorporation of additional data
-  bool (*enqueuePosition)(const estimatorKalmanStorage_t* storage, const positionMeasurement_t* position);
-  bool (*enqueueDistance)(const estimatorKalmanStorage_t* storage, const distanceMeasurement_t* distance);
+  bool (*enqueuePosition)(const estimatorKalmanStorage_t* storage, const positionMeasurement_t position);
+  bool (*enqueueDistance)(const estimatorKalmanStorage_t* storage, const distanceMeasurement_t distance);
 
   void (*getPosition)(const estimatorKalmanStorage_t* storage, point_t* position);
   void (*getState)(const estimatorKalmanStorage_t* storage, state_t* state);
